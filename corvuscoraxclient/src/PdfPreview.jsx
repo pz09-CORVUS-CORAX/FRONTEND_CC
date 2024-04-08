6aa61e93a7c7b4608cbc2fe5a2725c87654cc069
ja669nle2bil <jakubbilinski.info@gmail.com>
2024-04-08 09:48:42 +0200

    frontend preview WIP

--- corvuscoraxclient/src/PdfPreview.jsx
+++ corvuscoraxclient/src/PdfPreview.jsx
@@ -1,28 +1,52 @@
 import React, { useState } from 'react';
-import { Viewer, SpecialZoomLevel } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { Document, Page, pdfjs } from 'react-pdf'
+import samplePDF from './pdfresizer.com-pdf-crop.pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const PdfPreview = () => {
     const [file, setFile] = useState(null);
+    const [numPages, setNumPages] = useState(null);
 
     const handleChange = (event) => {
         const selectedFile = event.target.files[0];
         setFile(selectedFile);
     };
 
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    }
     return (
         <div>
-            <input type="file" onChange={handleChange} />
-            {file && (
-                <div style={{ marginTop: '20px' }}>
-                    <Viewer fileUrl={URL.createObjectURL(file)}>
-                        <SpecialZoomLevel scale={1.5} />
-                        {defaultLayoutPluginInstance => defaultLayoutPluginInstance.defaultLayoutPlugin}
-                    </Viewer>
-                </div>
-            )}
+          <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
+                {Array.from(new Array(numPages), (el, index) => (
+                  <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                ))}
+          </Document>
         </div>
-    );
-};
+      );
+    };
 
+//     return (
+//         <div>
+//             <input type="file" onChange={handleChange} />
+//             {file ? (
+//                 <div style={{ marginTop: '20px' }}>
+//                     <Document file={URL.createObjectURL(file)} onLoadSuccess={onDocumentLoadSuccess}>
+//                         {Array.from(new Array(numPages), (el, index) => {
+//                             <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+//                         })}
+//                     </Document>
+//                 </div>
+//             ) : ( //default to the sample pdf)
+//                 <div style={{ marginTop: '20px' }}>
+//                     <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
+//                         {Array.from(new Array(numPages), (el, index) => {
+//                             <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+//                         })}
+//                     </Document>
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
 export default PdfPreview;
