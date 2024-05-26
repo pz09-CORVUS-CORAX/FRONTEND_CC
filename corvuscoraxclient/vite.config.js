822f85acb9dd69e04f7f8ab0fa987dbaeb0313b2
b1lbudwebinho <czysteszalenstwopozdro@gmail.com>
2024-05-26 17:13:57 +0200

    Fix/ external port binding.

--- corvuscoraxclient/vite.config.js
+++ corvuscoraxclient/vite.config.js
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/',
   define: {
     global: 'window',
   },
-})
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: parseInt(process.env.PORT) || 5173,
+  },
+});
